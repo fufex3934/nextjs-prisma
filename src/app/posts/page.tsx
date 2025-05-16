75bc2087d55517bc5f4a763cfed2fcc33d942ead
fufex3934 <fufawakgari174@gmail.com>
2025-05-16 10:31:51 +0300

    post page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Posts = () => {
   return (
-    <div>
-      <h1>Posts Page</h1>
-    </div>
-  )
-}
+    <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
+      <h1 className="text-3xl font-semibold">All Posts(0)</h1>
+      <ul className="border-t border-b border-black/10 py-5 leading-8"></ul>
+    </main>
+  );
+};
 
-export default Posts
+export default Posts;
