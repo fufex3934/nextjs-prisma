ff4d53fcf58bc4438e6583e7e6dde9e3fddf9bd9
fufex3934 <fufawakgari174@gmail.com>
2025-05-16 11:09:08 +0300

    fetching data by using prisma orm

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import Link from "next/link";
+import prisma from "@/lib/db";
+const Posts = async () => {
+  const posts = await prisma.post.findMany();
 
-const Posts = () => {
   return (
     <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
       <h1 className="text-3xl font-semibold">All Posts(0)</h1>
-      <ul className="border-t border-b border-black/10 py-5 leading-8"></ul>
+      <ul className="border-t border-b border-black/10 py-5 leading-8">
+        {posts.map((post) => (
+          <li key={post.id} className="flex items-center justify-between px-5">
+            <Link href={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
