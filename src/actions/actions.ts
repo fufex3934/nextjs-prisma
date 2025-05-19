fa41e99ef4b0be8142771d36c91189864fde729a
fufex3934 <fufawakgari174@gmail.com>
2025-05-19 10:15:02 +0300

    create post

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,8 +1,16 @@
 "use server";
 
 import prisma from "@/lib/db";
+import { revalidatePath } from "next/cache";
 
 export async function createPost(formData: FormData) {
+  const user = await prisma.user.findUnique({
+    where: { email: "fufa@gmail.com" }, 
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
   await prisma.post.create({
     data: {
       title: formData.get("title") as string,
@@ -10,6 +18,31 @@ export async function createPost(formData: FormData) {
         .replace(/\s+/g, "-")
         .toLowerCase(),
       content: formData.get("content") as string,
+      author:{
+        connect:{
+          email:user.email
+        }
+      }
     },
+  });
+  revalidatePath("/posts");
+}
+
+export async function editPost(formData: FormData, id: string) {
+  await prisma.post.update({
+    where: { id },
+    data: {
+      title: formData.get("title") as string,
+      slug: (formData.get("title") as string)
+        .replace(/\s+/g, "-")
+        .toLowerCase(),
+      content: formData.get("content") as string,
+    },
+  });
+}
+
+export async function deletePost(id: string) {
+  await prisma.post.delete({
+    where: { id },
   });
 }
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,19 +2,26 @@ import Link from "next/link";
 import prisma from "@/lib/db";
 import { createPost } from "@/actions/actions";
 const Posts = async () => {
-  const posts = await prisma.post.findMany();
+  const user = await prisma.user.findUnique({
+    where:{
+      email:'fufa@gmail.com'
+    },
+    include:{
+      posts:true
+    }
+  });
 
   return (
     <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
-      <h1 className="text-3xl font-semibold">All Posts({posts.length})</h1>
+      <h1 className="text-3xl font-semibold">All Posts({user?.posts.length})</h1>
       <ul className="border-t border-b border-black/10 py-5 leading-8">
-        {posts.map((post) => (
+        {user?.posts.map((post) => (
           <li key={post.id} className="flex items-center justify-between px-5">
             <Link href={`/posts/${post.slug}`}>{post.title}</Link>
           </li>
         ))}
       </ul>
-      <form action={createPost} className="flex flex-col gap-y-2 w-[300px] bg-white ">
+      <form action={createPost}   className="flex flex-col gap-y-2 w-[300px] bg-white ">
         <input
           type="text"
           name="title"
